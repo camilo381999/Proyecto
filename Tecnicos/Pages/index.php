a2fc104f4c6efe8fb1dc020911187c3dd027bed1
manuel927 <manuel.smo24@gmail.com>
2021-02-19 00:51:27 -0500

    inicios de bootstrap

--- Tecnicos/Pages/index.php
+++ Tecnicos/Pages/index.php
@@ -8,7 +8,7 @@ $ModeloUsuarios->validateSession();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
---
+++ principal.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tecniclick</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
