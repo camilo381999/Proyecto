91bd28ff9467c433c1478081cb9cbe2379eba654
camilo381999 <camilo-leguizamon1@upc.edu.co>
2021-02-21 22:26:50 -0500

    boostrap4 en barra y modificacion en login

--- ingresar.php
+++ ingresar.php
@@ -1,19 +1,38 @@
 <?php
     include_once('templates/iniciar-html.php');
+	include_once('templates/menu.php');
 ?>
- 	<h1>Inicio de Sesión</h1>
- 	<form method="POST" action="Usuarios/Controladores/Login.php">
- 		Usuario <br>
- 		<input type="text" name="Usuario" required="" autocomplete="off" placeholder="Usuario"> <br><br>
- 		Contraseña <br>
- 		<input type="password" name="Contrasena" required="" autocomplete="off" placeholder="Contraseña"><br><br>
- 		<input type="submit" value="Inicia Sesión">
- 	</form>
-	 <form method="POST" action="registro.php">
-	 <input type="submit" value="Registrarse">
- 	</form>
+
+<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-4 col-sm-4 col-xs-12"></div>
+			<div class="col-md-4 col-sm-4 col-xs-12">
+				<!-- form start -->
+				<form class="form-container" id="form-ingreso" autocomplete="off" method="POST" action="Usuarios/Controladores/Login.php">
+				
+						<div class="ingreso-title">
+						<h1>Inicio de Sesión</h1>
+						</div>
+				
+						<div class="form-group">
+							<input name="Usuario" type="text"  class="form-control" required="" autocomplete="off" placeholder="Usuario"> 
+							<span class="help-block" id="error"></span>                     
+						</div>
+									
+						<div class="form-group">
+							<input name="Contrasena" type="password" class="form-control" required="" autocomplete="off" placeholder="Contraseña">
+
+							<span class="help-block" id="error"></span>                     
+						</div>
+
+						<button type="submit" class="btn btn-primary btn-block">Inicia Sesión</button>
+						<a href="">¿Olvidó su contraseña?</a>
+				</form>
+			</div>
+			<div class="col-md-4 col-sm-4 col-xs-12"></div>
+		</div>
+	</div>
 	
-	<a href="">¿Olvidó su contraseña?</a>
 
 <?php
     include_once('templates/terminar-html.php');
--- templates/menu.php
+++ templates/menu.php
@@ -1,31 +1,29 @@
-<nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Despliega la barra de navegación</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Tecniclick</a>
-            </div>
-            <div class="navbar-collapse collapse" id="navbar">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">Técnicos</a>
-                    </li>
-                    <li>
-                        <a href="#">Preguntas frecuentes</a>
-                    </li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="ingresar.php">Iniciar sesión</a>
-                    </li>
-                    <li>
-                        <a href="registro.php">Registro</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+<nav class="navbar navbar-expand-md navbar-dark bg-primary">
+<div class="container">
+    <a class="navbar-brand" href="index.php">
+        <img src="img/logo.png" width="150" height="70" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li>
+                <a class="nav-link" href="#">Técnicos</a>
+            </li>
+            <li>
+                <a class="nav-link" href="#">Preguntas frecuentes</a>
+            </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li>
+                <a class="nav-link" href="ingresar.php">Iniciar sesión</a>
+            </li>
+            <li>
+                <a class="nav-link" href="registro.php">Registro</a>
+            </li>
+        </ul>
+    </div>
+</div>
+</nav>
