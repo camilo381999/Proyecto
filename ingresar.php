4cf28a49e07eb6b9a0ee65fd5ea9b2739a84c5fe
manuel927 <manuel.smo24@gmail.com>
2021-03-01 20:01:55 -0500

    Login completo

--- ingresar.php
+++ ingresar.php
@@ -1,4 +1,17 @@
 <?php
+
+	include_once('validarLog.php');
+
+	if (isset($_POST['ingresar'])){
+		$validar = new ValidarLogin($_POST['Correo'],
+		 $_POST['Contrasena']);
+
+		 if($validar -> obtener_error() === '' && !is_null($validar->getResult())){
+			header('Location: /Proyecto/index-Clientes.php');
+		 }
+	}
+
+
     include_once('templates/iniciar-html.php');
 	include_once('templates/menu.php');
 ?>
@@ -8,24 +21,33 @@
 			<div class="col-md-4 col-sm-4 col-xs-12"></div>
 			<div class="col-md-4 col-sm-4 col-xs-12">
 				<!-- form start -->
-				<form class="form-container" id="form-ingreso" autocomplete="off" method="POST" action="Usuarios/Login.php">
+				<form class="form-container" id="form-ingreso" autocomplete="off" method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>">
 				
 						<div class="ingreso-title">
 						<h1>Inicio de Sesión</h1>
 						</div>
 				
 						<div class="form-group">
-							<input name="Correo" type="text"  class="form-control" required="" autocomplete="off" placeholder="Correo"> 
-							<span class="help-block" id="error"></span>                     
+							<input name="Correo" type="email"  class="form-control" required="" autofocus placeholder="Correo" 
+							<?php
+								if (isset($_POST['ingresar']) && isset($_POST['Correo']) && !empty($_POST['Correo'])){
+									echo 'value="'. $_POST['Correo'] . '"';
+								}
+							?>>                     
+							
 						</div>
 									
 						<div class="form-group">
-							<input name="Contrasena" type="password" class="form-control" required="" autocomplete="off" placeholder="Contraseña">
-
-							<span class="help-block" id="error"></span>                     
+							<input name="Contrasena" type="password" class="form-control" required="" placeholder="Contraseña">                   
 						</div>
 
-						<button type="submit" class="btn btn-primary btn-block">Inicia Sesión</button>
+						<?php
+							if (isset($_POST['ingresar'])){
+								$validar -> mostrar_error();
+							}
+						?>
+
+						<button name="ingresar" type="submit" class="btn btn-primary btn-block">Inicia Sesión</button>
 						<a href="">¿Olvidó su contraseña?</a>
 				</form>
 			</div>
