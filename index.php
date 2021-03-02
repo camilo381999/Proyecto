310d5e3c9b2c33087b25398c2aaa4f019867fdc3
manuel927 <manuel.smo24@gmail.com>
2021-03-01 22:01:42 -0500

    Carousel index

--- index.php
+++ index.php
@@ -1,15 +1,30 @@
 <?php
-    include_once('templates/iniciar-html.php');
-    include_once('templates/menu.php');
+include_once('templates/iniciar-html.php');
+include_once('templates/menu.php');
 ?>
-    
-    <div class="container">
-        <div class="jumbotron">
-        <img src="img/logo.png" width="60%" height="60%" />
-            
-        </div>
-    </div>
+
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active" style="background-image:url('/Proyecto/img/MABE.jpg')">
+                        <!-- <img src="/Proyecto/img/MABE.jpg" class="d-block w-100" alt="..."> -->
+                    </div>
+                    <div class="carousel-item" style="background-image:url('/Proyecto/img/tecnico2.jpg')">
+<!--                         <img src="/Proyecto/img/tecnico2.jpg" class="d-block w-100" alt="...">
+ -->                    </div>
+                    <div class="carousel-item" style="background-image:url('/Proyecto/img/slider-linea-blanca.jpg')">
+<!--                         <img src="/Proyecto/img/slider-linea-blanca.jpg" class="d-block w-100" alt="...">
+ -->                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
 
 <?php
-    include_once('templates/terminar-html.php');
+include_once('templates/terminar-html.php');
 ?>
