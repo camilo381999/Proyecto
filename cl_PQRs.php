13b024863ef29f8c4a578a9297d9cedf9b7c123b
manuel927 <manuel.smo24@gmail.com>
2021-04-14 18:30:16 -0500

    funcionalidad de pqrs

--- cl_PQRs.php
+++ cl_PQRs.php
@@ -1,17 +1,34 @@
 <?php
 include_once("Usuarios.php");
+include_once("Publicacion.php");
 
 $ModeloUsuarios = new Usuarios();
 //Validar la sesion si es cliente o tecnico
 
-if($ModeloUsuarios->getPerfil()=='Usuario'){
+if ($ModeloUsuarios->getPerfil() == 'Usuario') {
     $ModeloUsuarios->validateSessionClientes();
-}else{
+} else {
     $ModeloUsuarios->validateSessionTecnicos();
 }
 
 include_once('templates/iniciar-html.php');
 include_once('templates/menu.php');
+
+if (isset($_POST['enviar'])) {
+    $Modelo = new Publicacion();
+    if ($Modelo->añadirPqr($_POST['Correo'], $_POST['Asunto'], $_POST['Descripcion'], $_POST['Nombre'])) {
+
+        //script del alert
+        echo "<script> Swal.fire({
+                title: 'Solicitud enviada',
+                text: 'Proximamente alguien se comunicara con usted!',
+              }).then(
+                function() {
+                    window.location.href = 'index.php';
+                });";
+        echo "</script>";
+    }
+}
 ?>
 
 <div class="publicacion-title">
@@ -42,19 +59,19 @@ include_once('templates/menu.php');
                     <label>¿Cómo podemos ayudarte?</label>
                     <select name="Asunto" class="form-control" required>
                         <option selected>-</option>
-                        <option value="1">Tengo una duda acerca de mi cuenta</option>
-                        <option value="2">Tengo problemas con un técnico</option>
-                        <option value="3">Tengo problemas con un cliente</option>
-                        <option value="4">Tengo problemas con la aplicación</option>
-                        <option value="5">Tengo problemas con la cuenta</option>
-                        <option value="6">Quiero reportar un problema técnico</option>
-                        <option value="7">Sugerencias</option>
+                        <option value="Tengo una duda acerca de mi cuenta">Tengo una duda acerca de mi cuenta</option>
+                        <option value="Tengo problemas con un técnico">Tengo problemas con un técnico</option>
+                        <option value="Tengo problemas con un cliente">Tengo problemas con un cliente</option>
+                        <option value="Tengo problemas con la aplicación">Tengo problemas con la aplicación</option>
+                        <option value="Tengo problemas con la cuenta">Tengo problemas con la cuenta</option>
+                        <option value="Quiero reportar un problema técnico">Quiero reportar un problema técnico</option>
+                        <option value="Sugerencias">Sugerencias</option>
                     </select>
                 </div>
 
                 <div class="form-group">
-                <label>Descripción</label>
-                    <textarea name="Descripcion" class="form-control"  cols="200" rows="3" required></textarea>
+                    <label>Descripción</label>
+                    <textarea name="Descripcion" class="form-control" cols="200" rows="3" required></textarea>
                 </div>
 
                 <div class="form-group">
